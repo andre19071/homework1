87365941ef73fa6c0802952eaa0c03c66aeccb46
Наталья <109668015+andre19071@users.noreply.github.com>
2022-08-10 14:19:44 +0700

    Решение задачи 4

--- task4/Program.cs
+++ task4/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход три числа и
+//выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Введите число a");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число с");
+int с = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (max < b) max = b;
+if (max < с) max = с;
+Console.Write("Максимальное число ");
+Console.WriteLine(max);
+
